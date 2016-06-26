9775a37a883f91fc64b13d3c344513a367f3f0ad
ZHQ <ZHQ@ZhuHongQing.lining.lngroup>
2016-06-26 21:32:51 +0800

    修正Steam移动导致的import问题
    修改原有创建方式到SPI的方式

--- zhq-generator/src/test/java/cn/zhuhongqing/generator/test/GenTest.java
+++ zhq-generator/src/test/java/cn/zhuhongqing/generator/test/GenTest.java
@@ -3,15 +3,21 @@ package cn.zhuhongqing.generator.test;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 import cn.zhuhongqing.Generator;
 
 public class GenTest {
 
-	public static void main(String[] args) {
+	@Test
+	@Ignore
+	public void test1() {
 		Map<String, Object> data = new HashMap<String, Object>();
 		data.put("name", "GenName");
 		data.put("data", "GenData");
-		Generator.gen("temp", "e:/gen", data);
+		Generator.gen("temp", "e:/gen2", data);
+		System.out.println("Gen success!");
 	}
 
 }
