c9a37e3143799e283d28c9c0056b4c4b3013f859
ZHQ <ZHQ@ZhuHongQing.lining.lngroup>
2016-06-26 21:28:40 +0800

    增加一个NotNull的注解

---
+++ zhq-core/src/main/java/cn/zhuhongqing/anno/NotNull.java
@@ -0,0 +1,24 @@
+package cn.zhuhongqing.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来描述一个参数或一个属性不能为Null
+ *
+ * @author HongQing.Zhu
+ *         <nl>
+ *         <li>Mail:qwepoidjdj(a)gmail.com</li>
+ *         <li>HomePage:www.zhuhongqing.cn</li>
+ *         <li>Github:github.com/legend0702</li>
+ *         </nl>
+ *
+ */
+
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotNull {
+
+}
